edf9c244ee81fad7761cfa2f26fd998dd56012e1
James McKay <dcseee-github@yahoo.com.au>
2014-10-24 19:02:18 +1100

    Initial commit.

---
+++ src/main/java/portablejim/additionalresources/AdditionalResources.java
@@ -0,0 +1,19 @@
+package portablejim.additionalresources;
+
+import net.minecraft.init.Blocks;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = AdditionalResources.MODID)
+public class AdditionalResources
+{
+    public static final String MODID = "additionalresources";
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+		// some example code
+        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+    }
+}
